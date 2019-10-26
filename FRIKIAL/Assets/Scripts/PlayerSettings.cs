96e5f27759ff84a4adc722a81a014e60f9f810b9
Prothoky <Prothoky@github.com>
2019-10-27 01:31:04 +0200

    Icons & Menus update

--- FRIKIAL/Assets/Scripts/PlayerSettings.cs
+++ FRIKIAL/Assets/Scripts/PlayerSettings.cs
@@ -1,13 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerSettings : MonoBehaviour
 {
  
-    void milibro(string optioname, bool b)
+    void changeplayersettings(string optioname, bool b)
     {
         PlayerPrefs.SetString(optioname,b.ToString());
     }
     
+    void update()
+    {
+        if(GameObject.Find("Toogle_Sound_Yes").GetComponent<Toggle>().isOn) GameObject.Find("Toogle_Sound_No").GetComponent<Toggle>().isOn = false;
+        else if(GameObject.Find("Toogle_Sound_No").GetComponent<Toggle>().isOn) GameObject.Find("Toogle_Sound_Yes").GetComponent<Toggle>().isOn = false;
+
+        if (GameObject.Find("Toogle_Voz_Assistant_Yes").GetComponent<Toggle>().isOn) GameObject.Find("Toogle_Voz_Assistant_No").GetComponent<Toggle>().isOn = false;
+        else if (GameObject.Find("Toogle_Voz_Assistant_No").GetComponent<Toggle>().isOn) GameObject.Find("Toogle_Voz_Assistant_Yes").GetComponent<Toggle>().isOn = false;
+
+        if (GameObject.Find("Toogle_Daltonic_Yes").GetComponent<Toggle>().isOn) GameObject.Find("Toogle_Daltonic_No").GetComponent<Toggle>().isOn = false;
+        else if (GameObject.Find("Toogle_Daltonic_No").GetComponent<Toggle>().isOn) GameObject.Find("Toogle_Daltonic_Yes").GetComponent<Toggle>().isOn = false;
+
+        if (GameObject.Find("Toogle_Vibration_Yes").GetComponent<Toggle>().isOn) GameObject.Find("Toogle_Vibration_No").GetComponent<Toggle>().isOn = false;
+        else if (GameObject.Find("Toogle_Vibration_No").GetComponent<Toggle>().isOn) GameObject.Find("Toogle_Vibration_Yes").GetComponent<Toggle>().isOn = false;
+    }
+
 }
