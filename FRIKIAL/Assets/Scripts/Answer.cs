85b85e477e7260c52e06014512074fbded093c64
Prothoky <Prothoky@github.com>
2019-10-27 23:21:28 +0100

    PRIMERA ENTREGA

---
+++ FRIKIAL/Assets/Scripts/Answer.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Answer : MonoBehaviour
+{
+
+    public GameObject ImageC;
+    public GameObject ImageW;
+
+    private void Start()
+    {
+        ImageC = GameObject.Find("CORRECT");
+        ImageW = GameObject.Find("CORRECT");
+        CloseImage(ImageC);
+        CloseImage(ImageW);
+    }
+
+    public void checkAnswer(string answer)
+    {
+        if (answer == "B")
+        {
+            OpenImage(ImageC);
+            CloseImage(ImageW);
+        }
+        else
+        {
+            OpenImage(ImageW);
+            CloseImage(ImageC);
+        }
+    }
+    
+    public void OpenImage(GameObject Image)
+    {
+        if(Image != null)
+        {
+            Image.SetActive(true);
+        }
+    }
+
+    public void CloseImage(GameObject Image)
+    {
+            Image.SetActive(false);
+    }
+
+}
